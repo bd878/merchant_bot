6c4650d6c469941fcaa78afdd3d35635458c15b1
bd878 <slavad878@gmail.com>
2025-03-29 20:42:19 +0300

    buttons

--- keyboards.go
+++ keyboards.go
@@ -6,10 +6,10 @@ import (
 )
 
 func TransactionsKeyboard(code LangCode, trans *models.StarTransactions) *models.InlineKeyboardMarkup {
-	keyboards := make([][]models.InlineKeyboardButton, 0)
+	keyboards := make([][]models.InlineKeyboardButton, 1)
 	for _, tr := range trans.Transactions {
-		keyboards = append(keyboards, []models.InlineKeyboardButton{
-			{Text: fmt.Sprintf("%d - %d", tr.Amount, tr.Date), CallbackData: "tr_" + tr.ID},
+		keyboards[0] = append(keyboards[0], models.InlineKeyboardButton{
+			Text: fmt.Sprintf("%d - %d", tr.Amount, tr.Date), CallbackData: "tr_" + tr.ID,
 		})
 	}
 	return &models.InlineKeyboardMarkup{
