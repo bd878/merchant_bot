18372faa2c64c8a23200a98905702e62b09b2434
bd878 <slavad878@gmail.com>
2025-03-29 20:46:21 +0300

    fix callback data

--- keyboards.go
+++ keyboards.go
@@ -6,10 +6,10 @@ import (
 )
 
 func TransactionsKeyboard(code LangCode, trans *models.StarTransactions) *models.InlineKeyboardMarkup {
-	keyboards := make([][]models.InlineKeyboardButton, 1)
+	keyboards := make([][]models.InlineKeyboardButton, 0)
 	for _, tr := range trans.Transactions {
-		keyboards[0] = append(keyboards[0], models.InlineKeyboardButton{
-			Text: fmt.Sprintf("%d - %d", tr.Amount, tr.Date), CallbackData: "tr_" + tr.ID,
+		keyboards = append(keyboards, []models.InlineKeyboardButton{
+			{Text: fmt.Sprintf("%d - %d", tr.Amount, tr.Date), CallbackData: "trans"},
 		})
 	}
 	return &models.InlineKeyboardMarkup{
